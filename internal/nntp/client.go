37f9f93d25cc2b62e35819134b55ca3fae042e1a
JRP <jrpacheco.dev@gmail.com>
2026-05-22 13:29:46 +0100

    fix(nntp): fix Windows compilation error for socket buffers configuration

--- internal/nntp/client.go
+++ internal/nntp/client.go
@@ -23,7 +23,6 @@ import (
 	"github.com/sirrobot01/decypharr/internal/retry"
 	"github.com/sirrobot01/decypharr/internal/utils"
 	"github.com/sourcegraph/conc/pool"
-	"golang.org/x/sys/unix"
 )
 
 // ProviderPool manages connections for a single provider using a LIFO stack
@@ -679,12 +678,7 @@ func (c *Client) socketControl() func(network, address string, rc syscall.RawCon
 	}
 	return func(_, _ string, rc syscall.RawConn) error {
 		return rc.Control(func(fd uintptr) {
-			if rb > 0 {
-				_ = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_RCVBUF, rb)
-			}
-			if wb > 0 {
-				_ = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_SNDBUF, wb)
-			}
+			_ = setSocketBuffers(fd, rb, wb)
 		})
 	}
 }
